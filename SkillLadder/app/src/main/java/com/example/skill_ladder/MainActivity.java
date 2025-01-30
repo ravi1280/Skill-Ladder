4b51282754efb4b46c93986149053e2dcffca61e
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-01-30 18:06:45 +0530

    Create Custom Alert

--- SkillLadder/app/src/main/java/com/example/skill_ladder/MainActivity.java
+++ SkillLadder/app/src/main/java/com/example/skill_ladder/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.skill_ladder;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.dynamicanimation.animation.DynamicAnimation;
+import androidx.dynamicanimation.animation.FlingAnimation;
+import androidx.dynamicanimation.animation.SpringAnimation;
+import androidx.dynamicanimation.animation.SpringForce;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +27,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        ImageView imageView = findViewById(R.id.SplashimageView);
+        SpringAnimation springAnimation = new SpringAnimation(imageView, DynamicAnimation.TRANSLATION_Y);
+
+        SpringForce springForce = new SpringForce();
+        springForce.setStiffness(SpringForce.STIFFNESS_LOW);
+        springForce.setDampingRatio(SpringForce.DAMPING_RATIO_HIGH_BOUNCY);
+        springForce.setFinalPosition(700f);
+
+        springAnimation.setSpring(springForce);
+        springAnimation.start();
+        new Handler().postDelayed(()->{
+
+            Intent intent = new Intent(MainActivity.this,TestActivity.class);
+            startActivity(intent);
+            finish();
+        },2000);
+
     }
 }
--- SkillLadder/app/src/main/java/com/example/skill_ladder/TestActivity.java
+++ SkillLadder/app/src/main/java/com/example/skill_ladder/TestActivity.java
@@ -12,23 +12,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.skill_ladder.model.customAlert;
 
-
-
-public class UserLoginActivity extends AppCompatActivity {
+public class TestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_user_login);
+        setContentView(R.layout.activity_test);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-
-
-
+        Button button = findViewById(R.id.TestAlert);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                customAlert.showCustomAlert(TestActivity.this, "Success", "Your operation was successful!",R.drawable.cancel);
+            }
+        });
     }
 }
