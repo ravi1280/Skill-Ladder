aba0cdc39424cf5dbb2a2b7bfebc65be150cfd3f
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-02-04 14:24:29 +0530

    update fix code

--- SkillLadder/app/src/main/java/com/example/skill_ladder/CompanyUpdateProfileActivity.java
+++ SkillLadder/app/src/main/java/com/example/skill_ladder/CompanyUpdateProfileActivity.java
@@ -1,12 +1,19 @@
 package com.example.skill_ladder;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.skill_ladder.model.customAlert;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class CompanyUpdateProfileActivity extends AppCompatActivity {
 
@@ -19,6 +26,48 @@ public class CompanyUpdateProfileActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        ImageView imageView01 = findViewById(R.id.CompanyProfileBackIcon);
+        imageView01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        Button showBottomSheetButton = findViewById(R.id.CompanyProfileBtn02);
+
+        showBottomSheetButton.setOnClickListener(v -> {
+
+            View bottomSheetView = getLayoutInflater().inflate(R.layout.update_password_bottom, null);
+            BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(CompanyUpdateProfileActivity.this);
+            bottomSheetDialog.setContentView(bottomSheetView);
+
+
+            Button actionOne = bottomSheetView.findViewById(R.id.UserUpdatePasswordBtn);
+            EditText text01 = bottomSheetView.findViewById(R.id.UserOldPassword);
+            EditText text02 = bottomSheetView.findViewById(R.id.UserNewPassword);
+            EditText text03 = bottomSheetView.findViewById(R.id.UserReTypePassword);
+
+            actionOne.setOnClickListener(view -> {
+                String oldpassword = text01.getText().toString().trim();
+                String newPassword = text02.getText().toString().trim();
+                String reNewPassword = text03.getText().toString().trim();
+
+                if (oldpassword.isEmpty()) {
+                    customAlert.showCustomAlert(CompanyUpdateProfileActivity.this, "Error", "Please Fill Old Password!", R.drawable.cancel);
+                } else if (newPassword.isEmpty()) {
+                    customAlert.showCustomAlert(CompanyUpdateProfileActivity.this, "Error", "Please Fill New Password!", R.drawable.cancel);
+                } else if (reNewPassword.isEmpty()) {
+                    customAlert.showCustomAlert(CompanyUpdateProfileActivity.this, "Error", "Please Fill Re-Type Password!", R.drawable.cancel);
+                } else {
+                    customAlert.showCustomAlert(CompanyUpdateProfileActivity.this, "Success", "Password Updated Successfully!", R.drawable.checked);
+                    bottomSheetDialog.dismiss();
+                }
+            });
+
+            bottomSheetDialog.show();
         });
     }
 }
