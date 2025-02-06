381475ad08088ef2b2f66fb13ad020ba76bb06af
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-02-06 14:38:34 +0530

    Custom Spinner and alert connection Error

--- SkillLadder/app/src/main/java/com/example/skill_ladder/GetStartActivity.java
+++ SkillLadder/app/src/main/java/com/example/skill_ladder/GetStartActivity.java
@@ -1,8 +1,11 @@
 package com.example.skill_ladder;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +14,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.skill_ladder.model.customAlert;
+
 public class GetStartActivity extends AppCompatActivity {
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,26 +29,33 @@ public class GetStartActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        if(!isConnected()){
+            customAlert.showCustomAlert(GetStartActivity.this,"Connection Error ","Please Check Your Network Connection !",R.drawable.no_wifi);
+        }else {
+//            Toast.makeText(GetStartActivity.this,"Welcome to app",Toast.LENGTH_SHORT).show();
+            CardView cardView01 = findViewById(R.id.getStartCardView01);
+            cardView01.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent01 = new Intent(GetStartActivity.this,UserLoginActivity.class);
+                    startActivity(intent01);
+                }
+            });
+            CardView cardView02 = findViewById(R.id.getStartCardView02);
+            cardView02.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent02 = new Intent(GetStartActivity.this,CompanyLogInActivity.class);
+                    startActivity(intent02);
+                }
+            });
+        }
 
-        CardView cardView01 = findViewById(R.id.getStartCardView01);
-        cardView01.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent01 = new Intent(GetStartActivity.this,UserLoginActivity.class);
-                startActivity(intent01);
-//                Intent intent01 = new Intent(GetStartActivity.this,LessonSuccessActivity.class);
-//                startActivity(intent01);
-            }
-        });
-        CardView cardView02 = findViewById(R.id.getStartCardView02);
-        cardView02.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                Intent intent02 = new Intent(GetStartActivity.this,CompanyLogInActivity.class);
-//                startActivity(intent02);
-                Intent intent02 = new Intent(GetStartActivity.this,CompanyLogInActivity.class);
-                startActivity(intent02);
-            }
-        });
+
+
+    }
+    private Boolean isConnected(){
+        ConnectivityManager connectivityManager=(ConnectivityManager)getApplicationContext().getSystemService(context.CONNECTIVITY_SERVICE);
+        return connectivityManager.getActiveNetworkInfo()!=null && connectivityManager.getActiveNetworkInfo().isConnectedOrConnecting();
     }
 }
