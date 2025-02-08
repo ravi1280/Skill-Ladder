c4f897f043bd224b8999850dd98a5bbe402c4ec3
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-02-08 12:06:27 +0530

    Add Firebase dependency

--- SkillLadder/app/build.gradle.kts
+++ SkillLadder/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -43,6 +44,8 @@ dependencies {
     implementation ("com.airbnb.android:lottie:6.0.0")
     implementation ("androidx.cardview:cardview:1.0.0")
     implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.9.0"))
+    implementation("com.google.firebase:firebase-firestore")
 
 //    implementation("com.github.LottieFiles:dotlottie-android:0.5.0")
 
